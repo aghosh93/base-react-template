5b81dfaf50089c0202e0016ec4748475d5f42fc7
Anupam Ghosh <anupam.ghosh93@gmail.com>
2019-01-09 01:46:05 -0500

    Adding hash routing instead of browser

--- server.js
+++ server.js
@@ -1,20 +1,21 @@
 const express = require('express');
 const webpack = require('webpack');
 const webpackDevMiddleware = require('webpack-dev-middleware');
-const webpackHotMiddleware = require("webpack-hot-middleware")
+const webpackHotMiddleware = require('webpack-hot-middleware');
 
 const config = require('./webpack.config');
+
 const compiler = webpack(config);
 
 const app = express();
 const port = 8080;
 
 app.use(webpackDevMiddleware(compiler, {
-  publicPath: config.output.publicPath
+  publicPath: config.output.publicPath,
 }));
 
 app.use(webpackHotMiddleware(compiler));
 
-app.listen(port, function () {
-  console.log(`Example app listening on port ${port}!\n`);
+app.listen(port, () => {
+  console.log(`Listening on port: ${port}!\n`);
 });
--- src/js/components/Main.react.js
+++ src/js/components/Main.react.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {
-  BrowserRouter as Router, Route, Switch, Link,
+  HashRouter as Router, Route, Switch, Link,
 } from 'react-router-dom';
 
 import Loading from './Loading.react';
